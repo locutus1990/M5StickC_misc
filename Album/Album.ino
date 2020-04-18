012c2e089fe2c2f16d2c7d7362ef6adee14c6aca
KURINO Takashi <pp_jiji@ybb.ne.jp>
2020-04-18 17:23:22 +0900

    Add a new project "Album"

---
+++ Album/Album.ino
@@ -0,0 +1,80 @@
+/*
+ * @file		Album.ino
+ * @brief		M5StickCに複数の画像を表示する(Button-A)
+ */
+#include <M5StickC.h>
+#include "images.h"
+
+namespace{
+	TFT_eSprite gTftSprite = TFT_eSprite(&M5.Lcd);
+}
+ 
+/*------------------------------------------------------------
+ * @fn			Slide image
+ * @brief		画像のトランジション表示
+ *------------------------------------------------------------*/
+void slideImage()
+{
+	static int idx=0;
+	int nextIdx = (idx+1)%imgCount;
+
+#if 1
+	//Slide
+	for(int x=0;x<8;x++)
+	{
+		double th = M_PI*(x/7.0)/2.0;
+		int xx = (int)(160*sin(th));
+		gTftSprite.pushImage(0,0,img_width,img_height,images[nextIdx]);
+		gTftSprite.pushImage(xx,0,img_width,img_height,images[idx]);
+		gTftSprite.pushSprite(0,0);
+		delay(30);
+	}
+#else
+	//Simple refresh
+	gTftSprite.pushImage(0,0,img_width,img_height,images[nextIdx]);
+	gTftSprite.pushSprite(0,0);
+#endif
+
+	idx = (idx+1)%imgCount;
+}
+
+/*------------------------------------------------------------
+ * @fn		setup
+ * @brief	セットアップ。スプラッシュ画面（緑色）と最初の画像を表示する
+ *------------------------------------------------------------*/
+void setup(void)
+{
+	M5.begin();
+
+	M5.Lcd.setRotation(1);
+	M5.Lcd.setSwapBytes(false);
+
+	gTftSprite.createSprite(img_width, img_height);
+	gTftSprite.setSwapBytes(true);
+
+	gTftSprite.setTextSize(2);
+	gTftSprite.setTextColor(TFT_BLACK);
+	gTftSprite.fillSprite( M5.Lcd.color565(32,128,32));
+	gTftSprite.setCursor(2, 40);
+	gTftSprite.print("Album start");  
+	gTftSprite.pushSprite(0,0);
+	delay(1000);
+
+	gTftSprite.pushImage(0,0,img_width,img_height,images[0]);
+	gTftSprite.pushSprite(0,0);
+}
+
+/*------------------------------------------------------------
+ * @fn		loop
+ * @brief	ループ処理。Aボタンが押されたら画像を切り替える
+ *------------------------------------------------------------*/
+void loop()
+{
+	M5.update();
+	if(M5.BtnA.isPressed())
+	{
+		slideImage();
+	}
+	delay(100); 
+}
+
---
+++ Album/imageConversion.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+* file     imageConversion.py
+* brief    フォルダ内の画像をArduino形式(rgb565)に変換する
+"""
+import os
+import sys
+import struct
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+#size of monitor
+LCD_W=160
+LCD_H=80
+
+#------------------------------
+#------------------------------
+def rgb2hex(rgb888):
+    """
+    rgb888をrgb565に変換する
+    Attributes:
+        rgb888 : rgb888の画素値[r, g, b]（１画素３バイト）
+        rgb565 : Arduino用の画素値（１画素２バイト）
+    """
+    r = rgb888[0]
+    g = rgb888[1]
+    b = rgb888[2]
+    rh = int(round((2**5-1)*(r/255.0)))<<11
+    gh = int(round((2**6-1)*(g/255.0)))<<5
+    bh = int(round((2**5-1)*(b/255.0)))
+    rgb565 = (rh+gh+bh)
+    return rgb565
+
+#------------------------------
+#------------------------------
+def resizeImage(srcImg):
+    """
+    画像ファイルをスクリーンに収まるように縮小する
+    Attributes:
+        srcImg : PIL.Image 形式の画像データ（サイズは任意）
+        dstImg : np.array 形式の画像データ(縮小後画像。rgb888)
+    """
+    src_w = srcImg.width
+    src_h = srcImg.height
+    #print(src_w, src_h)
+
+    if(src_w<src_w):
+        image = srcImg.rotate(90)
+        src_w = srcImg.width
+        src_h = srcImg.height
+
+    kw = LCD_W/src_w
+    kh = LCD_H/src_h
+    if(kw<kh):
+        dst_w = int(kw*src_w)
+        dst_h = int(kw*src_h)
+    else:
+        dst_w = int(kh*src_w)
+        dst_h = int(kh*src_h)
+
+    img = srcImg.resize((dst_w, dst_h))
+
+    dx = int( (LCD_W-dst_w)/2 )
+    wimg = Image.new('RGB', (LCD_W, LCD_H), (0xff, 0xff, 0xff))
+    wimg.paste(img, (dx, 0))
+    #print(src_w, src_h, dst_w, dst_h)
+    dstImg = np.array(wimg)
+
+    return dstImg
+
+
+#------------------------------
+#------------------------------
+def writeBinary(fout, img):
+    """
+    画像データをCのヘッダファイル形式でファイルに書き込む
+    Attributes:
+        fout : ファイルハンドラ
+        img : numpy.array形式の縮小後画像(rgb888)
+    """
+    ww = img.shape[1]
+    hh = img.shape[0]
+
+    cnt=0
+    fout.write( "{\n" )
+    for j in range(hh):
+        for i in range(ww):
+            rgb = img[j][i]
+            val = rgb2hex(rgb)
+            vv = format(val, "04x")
+            bytes_be = bytes.fromhex( vv )
+            bytes_le = bytes_be[::-1]
+            hex_le = bytes_le.hex();
+            fout.write( "0x"+hex_le+"," )
+            cnt = cnt+1
+            if(7<cnt):
+                fout.write( "\n" )
+                cnt=0
+    fout.write( "},\n" )
+                
+#------------------------------
+# main routine
+#------------------------------
+if __name__=="__main__":
+    """
+    メイン関数
+    * フォルダ名を引数として渡すと、フォルダ内の画像を全てをバイナリ化して
+        ヘッダファイルに書き込む。
+        （M5StickCの場合、フォルダ内の画像数は30枚程度が上限）
+    * フォルダ内の画像以外のファイルは無視される
+    """
+    if 1<len(sys.argv):
+        dname = sys.argv[1]
+
+    dnameArr = os.listdir(dname)
+    with open("images.h", mode="w") as fout:
+        fout.write("const uint16_t images[]["+str(LCD_W*LCD_H)+"]  = {\n")
+        imgCnt=0
+
+        for fname in dnameArr:
+            fname = dname + "/" + fname
+            print(fname)
+            try:
+                image = Image.open(fname)
+                img = resizeImage(image)
+                writeBinary(fout, img);
+                imgCnt=imgCnt+1
+            except IOError:
+                print("Read failed : "+str(fname));
+
+        fout.write("};\n")
+        fout.write("int img_width  = "+ str(LCD_W)+";\n")
+        fout.write("int img_height = "+ str(LCD_H)+";\n")
+        fout.write("int imgCount  = "+ str(imgCnt)+";\n")
+
